96f88a705e4cb866a39dbf205ab6e44c43a40915
gsousaaa <glaucoezequiel94@gmail.com>
2025-02-20 23:22:22 -0300

    fix: adding try catch to return error message

--- lambda/products/fetchProducts.ts
+++ lambda/products/fetchProducts.ts
@@ -29,12 +29,20 @@ export const handler = async (event: APIGatewayProxyEvent, context: Context): Pr
         const productId = event.pathParameters?.id!
         console.log(`GET /products/${productId}`)
 
-        const product = await productRepository.getProductById(productId)
-        return {
-            statusCode: 200,
-            body: JSON.stringify({
-                product
-            })
+        try {
+            const product = await productRepository.getProductById(productId)
+            return {
+                statusCode: 200,
+                body: JSON.stringify({
+                    product
+                })
+            }
+        } catch (err) {
+            console.error((<Error>err).message)
+            return {
+                statusCode: 404,
+                body: (<Error>err).message
+            }
         }
     }
 
