13ecfc07a3a521db2bc2142a685332c82b0753b1
gsousaaa <glaucoezequiel94@gmail.com>
2025-02-25 11:53:18 -0300

    fix: adding productUrl in Product interface

--- lambda/products/layers/productsLayer/nodejs/productRepository.ts
+++ lambda/products/layers/productsLayer/nodejs/productRepository.ts
@@ -6,7 +6,8 @@ export interface Product {
     model: string,
     code: string,
     price: number,
-    productName: string
+    productName: string,
+    productUrl: string
 }
 
 export class ProductRepository {
@@ -71,12 +72,13 @@ export class ProductRepository {
             },
             ConditionExpression: 'attribute_exists(id)',
             ReturnValues: "UPDATED_NEW", // retornar oq foi alterado
-            UpdateExpression: "set productName = :n, code = :c, price = :p, model = :m",
+            UpdateExpression: "set productName = :n, code = :c, price = :p, model = :m, productUrl = :u",
             ExpressionAttributeValues: {
                 ":n": product.productName,
                 ":c": product.code,
                 ":p": product.price,
                 ":m": product.model,
+                ":u": product.productUrl
             }
         }).promise()
 
