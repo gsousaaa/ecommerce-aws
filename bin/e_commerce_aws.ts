8ae156befb31ade72644380c785f18485a7bfc25
gsousaaa <glaucoezequiel94@gmail.com>
2025-03-11 20:51:41 -0300

    feat: initializing events ddb stack

--- bin/e_commerce_aws.ts
+++ bin/e_commerce_aws.ts
@@ -4,6 +4,7 @@ import { ECommerceApiStack } from "../lib/ecommerceApi-stack";
 import { ProductsAppLayersStack } from "../lib/productsAppLayers-stack";
 
 import 'dotenv/config';
+import { EventsDdbStack } from "lib/eventsDdb-stack";
 
 const app = new cdk.App()
 
@@ -17,12 +18,15 @@ const tags = {
     team: "GlaucoSousa"
 }
 
+const eventsDdbStack = new EventsDdbStack(app, "EventsDdb", {env, tags})
+
 const productsAppLayersStack = new ProductsAppLayersStack(app, "ProductsAppLayers", {env, tags })
 
-const productsAppStack = new ProductsAppStack(app, "ProductsApp", { env, tags })
+const productsAppStack = new ProductsAppStack(app, "ProductsApp", { env, tags, eventsDdb: eventsDdbStack.eventsDdb })
 
 //depende indiretamente da stack de layers
 productsAppStack.addDependency(productsAppLayersStack)
+productsAppStack.addDependency(eventsDdbStack)
 
 const eCommerceApiStack = new ECommerceApiStack(app, "ECommerceApi", { fetchProductsHandler: productsAppStack.fetchProductsHandler, adminProductsHandler: productsAppStack.adminProductsHandler, env, tags })
 
