7356cc770ee3113c191ab12722d3cc39f85790d0
gsousaaa <glaucoezequiel94@gmail.com>
2025-03-17 21:57:20 -0300

    feat: orders App stack

--- lib/ecommerceApi-stack.ts
+++ lib/ecommerceApi-stack.ts
@@ -4,10 +4,10 @@ import * as cdk from "aws-cdk-lib"
 import * as cwlogs from "aws-cdk-lib/aws-logs"
 import { Construct } from "constructs"
 
-
 interface ECommerceApiStackProps extends cdk.StackProps {
     fetchProductsHandler: lambdaNodeJs.NodejsFunction,
-    adminProductsHandler: lambdaNodeJs.NodejsFunction
+    adminProductsHandler: lambdaNodeJs.NodejsFunction,
+    ordersHandler: lambdaNodeJs.NodejsFunction
 }
 
 export class ECommerceApiStack extends cdk.Stack {
@@ -34,12 +34,45 @@ export class ECommerceApiStack extends cdk.Stack {
             }
         })
 
+        this.productsService(props, api)
+        this.ordersService(props, api)
+    }
+
+    private ordersService(props: ECommerceApiStackProps, api: apigateway.RestApi) {
+        const ordersIntegration = new apigateway.LambdaIntegration(props.ordersHandler)
+
+        //resource - /orders
+        const ordersResource = api.root.addResource('orders')
+
+        // Validador de requisição (apenas para DELETE)
+        const deleteOrderValidator = new apigateway.RequestValidator(this, 'DeleteRequestValidator', {
+            restApi: api,
+            validateRequestParameters: true,
+        });
+
+        //GET /orders
+        //GET /orders?email=${email}
+        //GET /orders?email=${email}&orderId=${orderId}
+        ordersResource.addMethod('GET', ordersIntegration)
+
+        // DELETE /orders
+        ordersResource.addMethod('DELETE', ordersIntegration, {
+            requestParameters: {
+                'method.request.querystring.email': true,
+                'method.request.querystring.orderId': true
+            },
+            requestValidator: deleteOrderValidator
+        })
+        // POST /orders
+        ordersResource.addMethod('POST', ordersIntegration)
+    }
+
+    private productsService(props: ECommerceApiStackProps, api: apigateway.RestApi) {
         const fetchProductsIntegration = new apigateway.LambdaIntegration(props.fetchProductsHandler)
 
         // GET /products
         const productsResource = api.root.addResource("products")
         productsResource.addMethod('GET', fetchProductsIntegration)
-
 
         // GET /products/{id}
         const productIdResource = productsResource.addResource("{id}")
@@ -49,7 +82,6 @@ export class ECommerceApiStack extends cdk.Stack {
 
         //POST /products
         productsResource.addMethod('POST', adminProductsIntegration)
-
 
         // PUT /products/{id}
         productIdResource.addMethod('PUT', adminProductsIntegration)
