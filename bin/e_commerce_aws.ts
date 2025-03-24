cf5d74911c52d356dd4ee9af862808a0e8a7d5a6
gsousaaa <glaucoezequiel94@gmail.com>
2025-03-24 07:36:17 -0300

    fix: fixing import stacks path

--- bin/e_commerce_aws.ts
+++ bin/e_commerce_aws.ts
@@ -5,8 +5,8 @@ import { ProductsAppLayersStack } from "../lib/productsAppLayers-stack";
 
 import 'dotenv/config';
 import { EventsDdbStack } from "../lib/eventsDdb-stack";
-import { OrderAppStack } from "lib/ordersApp-stack";
-import { OrdersAppLayersStack } from "lib/ordersAppLayers-stack";
+import { OrderAppStack } from "../lib/ordersApp-stack";
+import { OrdersAppLayersStack } from "../lib/ordersAppLayers-stack";
 
 const app = new cdk.App()
 
@@ -25,7 +25,6 @@ const eventsDdbStack = new EventsDdbStack(app, "EventsDdb", { env, tags })
 const productsAppLayersStack = new ProductsAppLayersStack(app, "ProductsAppLayers", { env, tags })
 
 const productsAppStack = new ProductsAppStack(app, "ProductsApp", { env, tags, eventsDdb: eventsDdbStack.eventsDdb })
-
 
 const ordersAppLayersStack = new OrdersAppLayersStack(app, 'OrdersAppLayers', { env, tags })
 const ordersAppStack = new OrderAppStack(app, 'OrdersApp', { env, tags, productsDdb: productsAppStack.productsDdb })
